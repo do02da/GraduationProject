dd6ae5bd0f61540d1e32a8014ec6ce3bfd39eac1
wellcom8 <raze0209@gmail.com>
2020-11-20 15:40:43 +0900

    파이 테스트

--- src/main/java/com/board/service/BoardServiceImpl.java
+++ src/main/java/com/board/service/BoardServiceImpl.java
@@ -81,7 +81,7 @@ public class BoardServiceImpl implements BoardService {
 		
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		
-		String fileRoot = "\\summernote_image\\";											// 저장될 외부 파일 경로
+		String fileRoot = "\\home\\pi\\summernote_image\\";											// 저장될 외부 파일 경로
 		String originalFileName = multipartFile.getOriginalFilename();						// 오리지날 파일명
 		String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	// 파일 확장자
 				
